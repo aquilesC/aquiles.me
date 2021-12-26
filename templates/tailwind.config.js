258bb08cbb46df55660ae565a611cd6cd2fb3442
Aquiles Carattino <aqui.carattino@gmail.com>
2021-12-26 12:26:45 +0100

    Made wikilinks thinner, changed some fonts and design of essays

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -27,11 +27,13 @@ module.exports = {
                 sans: ['geomanistregular'],
                 head: ['geomanistbold'],
                 strong: ['geomanistbook'],
+                title: ["sourceserifpro"],
+                serif: ["lusitana"],
             },
             boxShadow: {
-                link: '0 -4px 0 0 rgba(178, 245, 234, .7) inset',
+                link: '0 -2px 0 0 rgba(178, 245, 234, .7) inset',
                 linkhover: '0 -6px 0 0 rgba(178, 245, 234, .7) inset',
-                wikilink: '0 -4px 0 0 rgba(255, 208, 0, .9) inset',
+                wikilink: '0 -2px 0 0 rgba(255, 208, 0, .9) inset',
                 wikihover: '0 -12px 0 0 rgba(255, 208, 0, .6) inset',
             },
             backgroundImage: theme => ({
